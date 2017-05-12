d12c2149a5cac4501d6e0e2234a4f7869e9334a1
Chris Wilcoxson <chris@slushman.com>
2017-05-12 14:23:46 -0500

    Adds missing activator class file.

---
+++ includes/class-tout-buttons-activator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link 		https://www.slushman.com
+ * @since 		1.0.0
+ *
+ * @package 	Tout_Buttons
+ * @subpackage 	Tout_Buttons/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since 		1.0.0
+ * @package 	Tout_Buttons
+ * @subpackage 	Tout_Buttons/includes
+ * @author 		Slushman <chris@slushman.com>
+ */
+class Tout_Buttons_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since 		1.0.0
+	 */
+	public static function activate() {
+
+
+
+	} // activate()
+
+} // class
